3e3b22b06f77d09e45d1e596e6cf07a5e5933c15
Juan-Hernandez <juanchohernandez@hotmail.com>
2016-08-24 14:49:34 -0400

    Add new routines for calibration on grids, no JLD

--- src/grosscalibratenoJLD.jl
+++ src/grosscalibratenoJLD.jl
@@ -1,6 +1,6 @@
-module grosscalibratenoJLD
+module d1d2calibratenoJLD
 #
-addprocs(23)
+addprocs(25)
 
 
 push!(LOAD_PATH,pwd())
@@ -8,84 +8,92 @@ export basemodel
 
 using ReservesTypes
 
-defsumgrid=Array{Float64}(5)
-def2grid=Array{Float64}(5)
-sizegrid=25
-defsumgrid=collect(linspace(0.06, 0.14, 5))
-def2grid=collect(linspace(0.21, 0.29, 5))
+# par1 is such that proportinoal cost at y=0.9  is (y-y_def)/y= d1+0.9d2 = par1
+# par 2 is the derivative loss at y=0.9: d y_def/ dy = 1-d1-1.8d2 = 1 - par2  
+
+par1=collect(linspace(0.01, 0.1, 10))
+par2=collect(linspace(0.05, 1.0, 20))'
+
+
+def1grid=Array{Float64}(10,20)
+def2grid=Array{Float64}(10,20)
+sizegrid=length(def1grid)
+
+# def1 is just 2*par1-par2
+broadcast!(+, def1grid, 2*par1, -par2 )
+# def2=(par2-par1)/0.9
+broadcast!(+, def2grid, -par1./0.9, par2./0.9 ) 
 
 basecompuparams=ComputationParams(
 	# Output Grid Lenght
-	21,		# ynum::Int64
+	25,		# ynum::Int64
 	# Debt grid parameters
 	0.0,	# debtmin::Float64
-	1.02,	# debtmax::Float64
-	52,		# debtnum::Int64
+	1.0,	# debtmax::Float64
+	41,		# debtnum::Int64
 	# Reserves grid parameters
 	0.0,		# resmin::Float64
-	1.0, 	# resmax::Float64
-	51,		# resnum::Int64
+	0.9, 	# resmax::Float64
+	31,		# resnum::Int64
 	# Temporary (smoothing shock parameters)
-	0.008, 	# msigma::Float64
+	0.01, 	# msigma::Float64
 	2.0,	# msdwidth::Float64 (For convergence)
 	13,		# mnum::Int64
 	-100.0,	# thrmin::Float64
 	)
 
-# Temporary arrays to store last model solution. Helps with speed
-tempvaluepay=Array{Float64}(basecompuparams.debtnum, basecompuparams.resnum, basecompuparams.mnum, basecompuparams.ynum, 2)
-tempvaluedefault=Array{Float64}(basecompuparams.resnum, basecompuparams.ynum, 2)
-tempbondprice=Array{Float64}(basecompuparams.debtnum, basecompuparams.resnum, basecompuparams.ynum, 2)
-
-basesolverparams=SolverParams(0.25, 0, 100, 3000, 5000, false, 1e-05)
-
 for i=1:sizegrid
-
+	
+	# 2. Solve model
+	
+	# 2.1. Set econ params
+	
 	baseeconparams=EconParams(
-		# Consumer
-		0.9875,	# bbeta::Float64
-		2,		# ggamma::Int64;  # HAS TO BE EQUAL TO 2. This cannot change. Will destroy threshold solution.
-		# Risk free rate
-		0.01, 	# rfree::Float64 # 4% yearly
-		# Bond Maturity and coupon
-		0.05, 	# llambda::Float64 # 5 year avg maturity
-		0.0185, 	# coupon:: Float64 
-		# Expected Output grid parameters
-		0.8, 	# logOutputRho::Float64
-		0.0716, 	# logOutputSigma::Float64
-		# Default output cost parameters
-		defsumgrid[div(i-1,5)+1]-def2grid[mod1(i,5)],# defcost1::Float64
-		def2grid[mod1(i,5)], 	# defcost2::Float64
-		0.1, # reentry::Float64
-		# Sudden Stop Probability
-		24.0, 	# panicfrequency::Float64 -- One every 16 quarters
-		10.0   # panicduration::Float64 -- 8 quarters
+				# Consumer
+		0.985,							# bbeta::Float64
+		2,								# ggamma::Int64;  # HAS TO BE EQUAL TO 2. This cannot change. Will destroy threshold solution.
+				# Risk free rate
+		0.01, 							# rfree::Float64 # 4% yearly
+				# Bond Maturity and coupon
+		0.05, 							# llambda::Float64 # 5 year avg maturity
+		0.0185, 						# coupon:: Float64 
+				# Expected Output grid parameters
+		0.8, 							# logOutputRho::Float64
+		0.0716, 						# logOutputSigma::Float64
+				# Default output cost parameters
+		def1grid[i],					# defcost1::Float64
+		def2grid[i], 					# defcost2::Float6
+		0.1, 							# reentry::Float64
+				# Sudden Stop Probability
+		32.0, 							# panicfrequency::Float64 -- One every 32 quarters
+		8.0   							# panicduration::Float64 -- 8 quarters
 		)
 	
+	# 2.2 Intialize model object
+	
 	basemodel=ReservesModel(basecompuparams,baseeconparams)
-	if i==1
-		modelinitialize!(basemodel)
-	else
-		setindex!(basemodel.valuepay, tempvaluepay, :)
-		setindex!(basemodel.valuedefault, tempvaluedefault, :)
-		setindex!(basemodel.bondprice, tempbondprice, :)
-	end
+	modelinitialize!(basemodel)
+	
+	# 2.3. Set solver parameters and call solver routine
+	basesolverparams=SolverParams(0.25, 0, 200, 1000, 5000, false, 1e-05)
 
 	solvereservesmodel!(basemodel, basesolverparams)	
 	
-	setindex!(tempvaluepay,basemodel.valuepay,:)
-	setindex!(tempvaluedefault,basemodel.valuedefault,:)
-	setindex!(tempbondprice,basemodel.bondprice,:)
-	# Simulate model
+	# 3. Simulate model
+	
 	basesimul=ModelSimulation(100000)
 	simulatemodel!(basesimul,basemodel,true)
+	
 	# 4. Obtain moments
 	basemoments=ModelMoments()
 	flag=getmoments!(basemoments, basesimul, basemodel.grids, 1000) # burnin 1000 observations
+	
+	# 5. Print relevans moments
 	println("---------------------------------------------------------------------------------------------")
 	println("  index  |    debt    |  reserves  |   spravg   |   sprvar   |    defstat  |  defchoice  |")
+	print("     ")
 	showcompact(i)
-	print("  |  ")
+	print("     |  ")
 	showcompact(basemoments.debtmean)
 	print("  | ")
 	showcompact(basemoments.reservesmean)
