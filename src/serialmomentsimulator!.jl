e6cd0fdb669ebba90714e7d33dd76843b58100e5
Juan Hernandez <juanchohernandez@hotmail.com>
2016-11-01 15:06:08 -0400

    Added serial routines for Sobol (grid) calibration

---
+++ src/serialmomentsimulator!.jl
@@ -0,0 +1,49 @@
+function serialmomentsimulator!(compuparams::ComputationParams, econparams::EconParams, solverparams::SolverParams, outfilename::AbstractString)
+	
+	innermodel=ReservesModel(compuparams,econparams)
+	modelinitialize!(innermodel)
+	
+	# 2.3. # Call solver routine
+
+	(resiternum,valuegap,pricegap,defaultgap)=solvereservesmodelserial!(innermodel, solverparams)	
+	
+	# 3. Simulate model with seed=true
+	
+	innersimul=ModelSimulation(100000)
+	simulatemodel!(innersimul,innermodel,true)
+	
+	# 4. Obtain moments
+	innermoments=ModelMoments()
+	flag=getmoments!(innermoments, innersimul, innermodel.grids, 1000) # burnin 1000 observations
+	
+	# 5. Return relevant moments
+	calout=open(outfilename,"a")
+		showcompact(calout, econparams.bbeta)
+		print(calout, " |  ")
+		showcompact(calout, econparams.defcost1)
+		print(calout, " |  ")
+		showcompact(calout, econparams.defcost2)
+		print(calout, " |  ")
+		showcompact(calout, econparams.panicfrequency)
+		print(calout, " |  ")
+		showcompact(calout, innermoments.debtmean)
+		print(calout, "  | ")
+		showcompact(calout, innermoments.reservesmean)
+		print(calout, "  | ")
+		showcompact(calout, innermoments.spreadmean)
+		print(calout, " | ")
+		showcompact(calout, innermoments.spreadsigma)
+		print(calout, " | ")
+		showcompact(calout, innermoments.defaultstatemean)
+		print(calout, "  | ")
+		showcompact(calout, innermoments.defaultchoicemean)
+		print(calout, "  | ")
+		showcompact(calout, innermoments.spreadXgrowth)
+		print(calout, "  | ")
+		showcompact(calout, maximum([valuegap,pricegap,defaultgap]) )
+		println(calout, "  |")
+	close(calout)
+	return nothing
+end	
+
+
---
+++ src/serialsobol.jl
@@ -0,0 +1,81 @@
+# This is now a script
+#module serialsobol
+#
+# addprocs(25)
+
+
+push!(LOAD_PATH,pwd())
+# export basemodel
+
+@everywhere using Base.LinAlg.BLAS
+@everywhere using ReservesTypes
+using Sobol
+
+
+@everywhere include("serialmomentsimulator!.jl")
+@everywhere include("solvereservesmodelserial!.jl")
+# 1. Define parameters
+growth=0.0		# Quarterly growth rate
+pivot=0.974 	# Pivot point for default cost
+
+# 1.1 Sobol points over calibrated parameter box 
+
+# Vector [beta, par1, par2, 1/freq]
+calsequence=SobolSeq(4, [0.985, -0.01, 0.1, 0.025 ], [0.99, 0.07, 1.0, 0.0625])
+# par1 is such that proportinoal cost at y=pivot  is (y-y_def)/y= d1+pivot*d2 = par1
+# par 2 is the derivative loss at y=pivot: d y_def/ dy = 1-d1-2*pivot*d2 = 1 - par2  
+
+# 1.2 Fixed computation parameters
+basecompuparams=ComputationParams(
+	# Output Grid Lenght
+	25,		# ynum::Int64
+	# Debt grid parameters
+	0.0,	# debtmin::Float64
+	1.0,	# debtmax::Float64
+	41,		# debtnum::Int64
+	# Reserves grid parameters
+	0.0,		# resmin::Float64
+	0.75, 	# resmax::Float64
+	31,		# resnum::Int64
+	# Temporary (smoothing shock parameters)
+	0.00625, 	# msigma::Float64
+	2.0,	# msdwidth::Float64 (For convergence)
+	13,		# mnum::Int64
+	-100.0,	# thrmin::Float64
+	)
+
+# 1.3 Solver parameters
+basesolverparams=SolverParams(0.25, 0, 0, 1000, 5000, false, 1e-05)
+
+# 1.4 Itereation control and output print
+iternum=0
+itermax=256
+outfilename="real974pivot.txt"
+calout=open(outfilename,"a")
+println(calout, "----------------------------------------------------------------------------------------------------------------------------------------------------")
+println(calout, "                  parvec                  |   debt    |  reserves  |   spravg   |   sprvar   |    defstat  |  defchoice  | sprXgrowth |   maxgap   |")
+close(calout)
+growth=0.00	# Quarterly growht rate
+pmap( serialmomentsimulator!, repeated(basecompuparams,itermax), 
+	[ baseeconparams=EconParams(
+		parvec[1]*(1+growth)^(1-2),		# bbeta::Float64
+		2,								# ggamma::Int64;  # HAS TO BE EQUAL TO 2. This cannot change. Will destroy threshold solution.
+		(0.01-growth)/(1+growth), 		# rfree::Float64 # 4% yearly
+		(0.05+growth)/(1+growth), 		# llambda::Float64 # 5 year avg maturity
+		0.0085+(0.01-growth)/(1+growth),# coupon:: Float64 # 340bp divided by 4 plus r (spread is the same with growth, r is not)
+		0.843, 							# logOutputRho::Float64
+		0.0223, 						# logOutputSigma::Float64
+		2*parvec[2]-parvec[3],			# defcost1::Float64
+		(parvec[3]-parvec[2])/pivot,	# defcost2::Float6
+		0.125,	 						# reentry::Float64
+		1.0/parvec[4], 					# panicfrequency::Float64 -- One every 32 quarters
+		8.0   							# panicduration::Float64 -- 8 quarters
+		)
+	for parvec in [next(calsequence) for id=1:itermax] ],
+	repeated(basesolverparams,itermax), repeated(outfilename,itermax) )	
+
+calout=open(outfilename,"a")	
+println(calout, "=============================================================================================")
+close(calout)
+#end # Module end
+
