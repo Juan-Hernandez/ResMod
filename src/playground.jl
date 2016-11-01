ca3e71e9478d131bc9e0838a63a65a8ab83854e9
Juan Hernandez <juanchohernandez@hotmail.com>
2016-11-01 18:07:19 -0400

    current baseline parameters

--- src/playground.jl
+++ src/playground.jl
@@ -2,20 +2,22 @@ module playground
 	export basemodel
 	push!(LOAD_PATH,pwd())
 	using ReservesTypes
-
+	using JLD
+	
+	growth=0.0065 # Avg quarterly growth
 	basecompuparams=ComputationParams(
 		# Output Grid Lenght
-		21,		# ynum::Int64
+		25,		# ynum::Int64
 		# Debt grid parameters
 		0.0,	# debtmin::Float64
-		1.02,	# debtmax::Float64
-		52,		# debtnum::Int64
+		1.0,	# debtmax::Float64
+		41,		# debtnum::Int64
 		# Reserves grid parameters
 		0.0,		# resmin::Float64
-		1.0, 	# resmax::Float64
-		51,		# resnum::Int64
+		0.75, 	# resmax::Float64
+		31,		# resnum::Int64
 		# Temporary (smoothing shock parameters)
-		0.008, 	# msigma::Float64
+		0.00625, 	# msigma::Float64
 		2.0,	# msdwidth::Float64u
 		13,		# mnum::Int64
 		-100.0,	# thrmin::Float64
@@ -23,22 +25,22 @@ module playground
 
 	baseeconparams=EconParams(
 		# Consumer
-		0.985,	# bbeta::Float64
-		2,		# ggamma::Int64;  # HAS TO BE EQUAL TO 2. This cannot change. Will destroy threshold solution.
+		0.991625*(1.0+growth)^(1-2),		# bbeta::Float64
+		2,							# ggamma::Int64;  # HAS TO BE EQUAL TO 2. This cannot change. Will destroy threshold solution.
 		# Risk free rate
-		0.01, 	# rfree::Float64 # 4% yearly
+		(0.01-growth)/(1.0+growth), 	# rfree::Float64 # 4% yearly
 		# Bond Maturity and coupon
-		0.05, 	# llambda::Float64 # 5 year avg maturity (6% avg quarterly debt service)
-		0.0185, 	# coupon:: Float64 
+		(0.05+growth)/(1.0+growth), 	# llambda::Float64 # 5 year avg maturity (6% avg quarterly debt service)
+		0.0085+(0.01-growth)/(1+growth),# coupon:: Float64 
 		# Expected Output grid parameters
-		0.8, 	# logOutputRho::Float64
-		0.0716, 	# logOutputSigma::Float64
+		0.7895, 	# logOutputRho::Float64
+		0.0702, 	# logOutputSigma::Float64
 		# Default output cost parameters
-		-0.1875,# defcost1::Float64
-		0.25, 	# defcost2::Float64
-		0.1, # reentry::Float64
+		-0.7825,# defcost1::Float64
+		0.9396, 	# defcost2::Float64
+		0.125, # reentry::Float64
 		# Sudden Stop Probability
-		24.0, 	# panicfrequency::Float64 -- One every 24 quarters (25% of the time in panic)
+		32.0, 	# panicfrequency::Float64 -- One every 24 quarters (25% of the time in panic)
 		8.0   # panicduration::Float64 -- 8 quarters
 		)
 	
@@ -49,9 +51,9 @@ module playground
 	basesolverparams=SolverParams(
 		0.25, 	# updatespeed::Float64 
 		0, 		# startiternum::Int64
-		1,		# interprint::Int64 
-		3000,	# itermax::Int64
-		1000, 	# intermediatesave::Int64
+		20,		# interprint::Int64 
+		1000,	# itermax::Int64
+		2000, 	# intermediatesave::Int64
 		false,	# policiesout::Bool
 		1e-5, 	# valtol::Float64 
 		)
@@ -62,28 +64,34 @@ module playground
 	# 3.1. Simulate model
 	basesimul=ModelSimulation(100000)
 	simulatemodel!(basesimul,basemodel)
+	# Save simulated
+	@save "firstsim.jld" basemodel basesimul
+
 	# 3.2. Obtain moments
 	basemoments=ModelMoments()
 	flag=getmoments!(basemoments, basesimul, basemodel.grids, 1000) # burnin 1000 observations
 	# 3.3 Print Moments
-	println("	debt	|	reserves	|	spravg		|	sprvar		|	defstat		|	defchoice	|")
-	println("---------------------------------------------------------------------------------------------")
-	showcompact(basemoments.debtmean)
-	print("	|	")
-	showcompact(basemoments.reservesmean)
-	print("	|	")
-	showcompact(basemoments.spreadmean)
-	print("	|	")
-	showcompact(basemoments.spreadsigma)
-	print("	|	")
-	showcompact(basemoments.defaultstatemean)
-	print("	|	")
-	showcompact(basemoments.defaultchoicemean)
-	println("	|	")
-	println("=============================================================================================")
+	println("	debt	|	reserves	|	spravg		|	sprvar		|	defstat		|	defchoice	| sprXgrowth |   maxgap   |")
+	println("----------------------------------------------------------------------------------------------------------------------")
+		showcompact(calout, innermoments.debtmean)
+		print(calout, "  | ")
+		showcompact(calout, innermoments.reservesmean)
+		print(calout, "  | ")
+		showcompact(calout, innermoments.spreadmean)
+		print(calout, " | ")
+		showcompact(calout, innermoments.spreadsigma)
+		print(calout, " | ")
+		showcompact(calout, innermoments.defaultstatemean)
+		print(calout, "  | ")
+		showcompact(calout, innermoments.defaultchoicemean)
+		print(calout, "  | ")
+		showcompact(calout, innermoments.spreadXgrowth)
+		print(calout, "  | ")
+		showcompact(calout, maximum([valuegap,pricegap,defaultgap]) )
+		println(calout, "  |")
+	println("======================================================================================================================")
 	# 3.4. Save solved
 	@save "firstsolved.jld" basemodel basesimul basemoments
-	
-	
+		
 end
 
